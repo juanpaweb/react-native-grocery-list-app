eabfdbff557c777b9123f8864fa6348f6bbe6543
snek <zachdevmail45@gmail.com>
2019-08-29 13:25:09 -0500

    State is now saved and persistent in AsyncStorage

--- redux/store.js
+++ redux/store.js
@@ -1,13 +1,34 @@
+import { AsyncStorage } from 'react-native';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { default as ReduxThunk } from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
 import { logger } from 'redux-logger';
 import rootReducer from './reducers/root';
 
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: [
+    'groceryList'
+  ],
+  blacklist: []
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+
 const configureStore = () => {
   return createStore(
-    rootReducer,
+    persistedReducer,
     compose(applyMiddleware(ReduxThunk, logger))
   );
 };
 
-export default configureStore;
+const store = configureStore();
+
+let persistor = persistStore(store);
+
+export {
+  store,
+  persistor,
+};
